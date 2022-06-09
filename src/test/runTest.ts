b8189a2c58e0249cc7d4fce30c1890bb19080dcd
Tyler James Leonhardt <me@tylerleonhardt.com>
2022-06-08 21:11:12 -0700

    Create node.js.yml (#13)
    * Create node.js.yml
    
    * Update and rename node.js.yml to ci.yml
    
    * misc
    
    * log
    
    * try macos
    
    * add windows
    
    * vsix upload

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -13,8 +13,9 @@ async function main() {
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath, version: 'insiders' });
+		await runTests({ extensionDevelopmentPath, extensionTestsPath, version: 'stable' });
 	} catch (err) {
+		console.error(err);
 		console.error('Failed to run tests');
 		process.exit(1);
 	}
